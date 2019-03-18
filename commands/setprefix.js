aced2591733e7fa45f9ffc5b477e96f1d3699e6c
Glitch (hello-express) <none>
2019-03-18 00:17:04 +0000

    👦🛰 Checkpoint ./commands/setprefix.js:1931475/247

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -3,10 +3,19 @@
  * @author Sankarsan Kampa (a.k.a k3rn31p4nic)
  * @license GPL-3.0
  */
+const fs = require('fs');
 const config = require("../config.json")
 
 exports.exec = async (message, args) => {
   
+ fs.editFile('./config.json', 'utf-8', function(err, data){
+    if (err) throw err;
+
+    console.log(typeof(data)); // string
+
+    data.replace(/\.\/+/, '-');
+
+}); 
 };
 
 exports.help = {
