989cbeb89dc9edf75b3bcf46783ad170caca5f56
Glitch (hello-express) <none>
2021-03-05 17:40:43 +0000

    🌑💡 Checkpoint ./steam.txt:28326974/112177 ./main.js:28326974/27

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ client.on('ready', () => {
     client.user.setStatus('available')
     client.user.setPresence({
         game: {
-            name: 'to donate do >donate',
+            name: '>help',
             type: "WATCHING"
         }
     });
