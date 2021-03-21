a1c966506b5443c2abc3b8805ebc180365a3674d
Glitch (hello-express) <none>
2021-03-21 20:16:58 +0000

    🎋🌕 Checkpoint ./commands/proxygen.js:28326974/2077 ./proxy.txt:28326974/276

---
+++ commands/proxygen.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const config = require("../config.json")
+const talkedRecently = new Set();
+
+exports.run = (client, message, args) => {
+
+    message.delete(message)
+
+    if (talkedRecently.has(message.author.id)) {
+        message.reply(`You need to wait **${config.cooldown}** minutes to __use__ this command again!`).then(m => {
+            setTimeout(() => {
+                m.delete(m)
+            }, 5000); //5 seconds
+        })
+    } else {
+        fs.readFile('./proxy.txt', function(err, data){
+            if(err) throw err;
+            data = data + '';
+            var lines = data.split('\n');
+            let random = lines[Math.floor(Math.random()*lines.length)];
+
+            let hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+            let embed = new Discord.RichEmbed()
+            .setTitle('__hq Proxy!__')
+            .addField('`Here is your hq proxy:`', `**${random}**`)
+            .addField('`Sent in server:`', `*${message.guild.name}*`)
+            .addField('`Sent in channel:`', `*${message.channel.name}*`)
+            .setThumbnail("https://tse3.mm.bing.net/th?id=OIP.CqiCSEvytOSG4_oFmX3gSQHaD4&pid=Api&P=0&w=312&h=165")
+            .setColor(hex)
+            .setTimestamp()
+            .setFooter('Have fun with it!')
+            message.author.send(embed)
+
+            message.reply(`Hey **${message.author.tag}**, I __successfully__ sent *your* **hq proxy** in DMS!`).then(m => {
+                setTimeout(() => {
+                    m.delete(m)
+                }, 5000); //5 seconds
+            })
+
+            talkedRecently.add(message.author.id);
+            setTimeout(() => {
+                talkedRecently.delete(message.author.id);
+            }, config.cooldown * 60 *1000);
+          
+        })
+    }
+}
